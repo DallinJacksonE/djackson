a5b1481cacee1e11552fab8e28e48023aad15d9f
Dallin Jackson <dallin@djackson.dev>
2025-09-09 21:52:06 -0600

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/notfound/notfound';
 import { Projects } from './pages/projects/projects.jsx';
+import { Navbar } from './navbar/navbar.jsx';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
